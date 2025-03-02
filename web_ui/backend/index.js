847f2605abaadb6e15b51ca07f021ed13b0ae4da
charlene-chenn <qcharlene8@gmail.com>
2025-03-02 00:30:39 +0000

    Update UI

---
+++ web_ui/backend/index.js
@@ -0,0 +1,94 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import AppRouter from './Router';
+
+// // Frontend rendering
+// ReactDOM.render(
+//     ReactDOM.render(
+//         <AppRouter />,
+//         document.getElementById('root')
+//     )
+// );
+
+// To connect with your mongoDB database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://charlenechenn:Atlas22331144!@cluster0.jbsx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    dbName: 'Cluster0',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+// Schema for users of app
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    language: {
+        type: String,
+        default: "English",
+        required: true,
+    }
+});
+const User = mongoose.model('users', UserSchema);
+User.createIndexes();
+
+// For backend and express
+const express = require('express');
+const app = express();
+const cors = require("cors");
+console.log("App listen at port 5001");
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, resp) => {
+
+    resp.send("App is Working");
+    // You can check backend is working or not by 
+    // entering http://loacalhost:5001
+    
+    // If you see App is working means
+    // backend working properly
+});
+
+app.post("/register", async (req, resp) => {
+    try {
+        const user = new User(req.body);
+        let result = await user.save();
+        result = result.toObject();
+        if (result) {
+            delete result.password;
+            resp.send(req.body);
+            console.log(result);
+        } else {
+            console.log("User already register");
+        }
+
+    } catch (e) {
+        resp.send("Something Went Wrong");
+    }
+});
+
+app.post("/login", async (req, resp) => {
+    try {
+        const user = await User.findOne({ name: req.body.name, password: req.body.password });
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send("Invalid credentials");
+        }
+    } catch (e) {
+        resp.send("Something Went Wrong");
+    }
+});
+app.listen(5001);
--- web_ui/src/App.js
+++ web_ui/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Route, Routess, useNavigate } from 'react-router-dom';
+import './App.css'; // Import the CSS file
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [password, setPW] = useState("");
+    const [number, setNumber] = useState("");
+    const [language, setLanguage] = useState("");
+    const [isRegister, setIsRegister] = useState(true);
+    const navigate = useNavigate();
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        const endpoint = isRegister ? 'register' : 'login';
+        let result = await fetch(`http://localhost:5001/${endpoint}`, {
+            method: "post",
+            body: JSON.stringify({ name, password, number, language }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result);
+        if (result) {
+          if (isRegister) {
+            setName("");
+            setPW("");
+            setNumber("");
+            setLanguage("");
+            alert("Registration successful");
+            navigate('/dashboard');
+          } else {
+              if (result.name) {
+                  alert("Login successful");
+                  navigate('/dashboard');
+              } else {
+                  alert("Invalid credentials, please register.");
+              }
+          }
+        }
+    };
+
+    return (
+        <div className="center-content">
+            <h1><img src="/huhai_logo.svg" alt="Huhai Logo" /></h1>
+            <div>
+                <button onClick={() => setIsRegister(true)}>Register</button>
+                <button onClick={() => setIsRegister(false)}>Login</button>
+            </div>
+
+            <form onSubmit={handleOnSubmit}>
+              {isRegister && (
+                  <>
+                      <input type="number" placeholder="number"
+                      value={number} onChange={(e) => setNumber(e.target.value)} />
+                      <input type="text" placeholder="language"
+                      value={language} onChange={(e) => setLanguage(e.target.value)} />
+                  </>
+              )}
+              <input type="text" placeholder="name" 
+              value={name} onChange={(e) => setName(e.target.value)} />
+              <input type="password" placeholder="password" 
+              value={password} onChange={(e) => setPW(e.target.value)} />
+              <br />
+              
+              <button type="submit">{isRegister ? 'Register' : 'Login'}</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
